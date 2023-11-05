bdfc7253325d6c27487289ce7915e30e7cbf9ca2
Dinoh665 <fruhaufandrei@gmail.com>
2023-11-04 22:43:04 -0300

    Consumindo API dos valores atuais das moedas

--- script.js
+++ script.js
@@ -2,45 +2,46 @@ const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
 
-function convertValues() {
+const convertValues = async() => {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
 
     console.log(currencySelect.value)
 
-    const dolarToday = 5.14
-    const euroToday = 5.45
-    const libraToday = 6.3
-    const bitToday = 143937.62
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,GBP-BRL").then( response => response.json())
 
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const libra = data.GBPBRL.high
+    const btc = data.BTCBRL.high
 
     if (currencySelect.value == "Dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
+        }).format(inputCurrencyValue / dolar)
     }
 
     if (currencySelect.value == "Euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
+        }).format(inputCurrencyValue / euro)
     }
 
     if (currencySelect.value == "Libra") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
             style: "currency",
             currency: "GBP"
-        }).format(inputCurrencyValue / libraToday)
+        }).format(inputCurrencyValue / libra)
     }
 
     if (currencySelect.value == "Bitcoin") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en", {
             style: "currency",
             currency: "BTC"
-        }).format(inputCurrencyValue / bitToday)
+        }).format(inputCurrencyValue / btc)
     }
 
 
